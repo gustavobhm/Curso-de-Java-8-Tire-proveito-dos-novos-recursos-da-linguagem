de8d9081e712c005535b04a0222227d3b24ee026
Gustavo Luis dos Santos <gustavobhm@bol.com.br>
2019-05-10 09:07:36 -0300

    Aula 3 - Código mais sucinto com Method references

--- java8/src/java8/OrdenaStrings.java
+++ java8/src/java8/OrdenaStrings.java
@@ -1,7 +1,9 @@
 package java8;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
 
 public class OrdenaStrings {
 
@@ -12,19 +14,28 @@ public class OrdenaStrings {
 		palavras.add("casa do código");
 		palavras.add("caelum");
 
-		palavras.sort((s1, s2) -> {
-			if (s1.length() < s2.length())
-				return -1;
-			if (s1.length() > s2.length())
-				return 1;
-			return 0;
-		});
+//		palavras.sort((s1, s2) -> {
+//			if (s1.length() < s2.length())
+//				return -1;
+//			if (s1.length() > s2.length())
+//				return 1;
+//			return 0;
+//		});
+//
+//		palavras.sort((s1, s2) -> s1.length() - s2.length());
 
-		palavras.sort((s1, s2) -> s1.length() - s2.length());
+		Function<String, Integer> funcao = s -> s.length();
+		Function<String, Integer> funcao2 = String::length;
+		Comparator<String> comparador = Comparator.comparing(funcao);
+		palavras.sort(comparador);
+
+		palavras.sort(Comparator.comparing(s -> s.length()));
+		palavras.sort(Comparator.comparing(String::length));
 
 		System.out.println(palavras);
 
 		palavras.forEach(palavra -> System.out.println(palavra));
+		palavras.forEach(System.out::println);
 
 	}
 
